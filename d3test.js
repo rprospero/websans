9f8321d75fc08a20878a410bf6378937cd329a8a
Adam Washington <Adam.L.Washington@gmail.com>
2014-01-07 17:40:08 -0500

    Moved to bootstrap flow

--- d3test.js
+++ d3test.js
@@ -47,7 +47,7 @@ var line = d3.svg.line()
     .y(function(d) {return y(d.y);})
     .interpolate("basis");
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select(".chart")
     .attr("width",width + margin.left + margin.right)
     .attr("height",height + margin.top + margin.bottom)
     .append("g")
